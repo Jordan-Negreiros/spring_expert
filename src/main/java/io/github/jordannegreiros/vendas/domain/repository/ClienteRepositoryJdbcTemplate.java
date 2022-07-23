297816bf179b41eaf212e60a01e899829b9fdbd4
jordan-negreiros <jordannegreirossantos@gmail.com>
2022-07-23 12:31:32 -0300

    JdbcTemplate

--- src/main/java/io/github/jordannegreiros/vendas/domain/repository/ClienteRepositoryJdbcTemplate.java
+++ src/main/java/io/github/jordannegreiros/vendas/domain/repository/ClienteRepositoryJdbcTemplate.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
-public class ClienteRepository {
+public class ClienteRepositoryJdbcTemplate {
 
     private static final String INSERT = "insert into cliente (nome) values (?)";
     private static final String UPDATE = "update CLIENTE set nome = ? where id = ?";
