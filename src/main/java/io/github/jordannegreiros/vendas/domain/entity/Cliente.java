b3894851cd59c81df0c80d85329babc0a383ff35
jordan-negreiros <jordannegreirossantos@gmail.com>
2022-07-23 13:03:38 -0300

    add jpa mapping

--- src/main/java/io/github/jordannegreiros/vendas/domain/entity/Cliente.java
+++ src/main/java/io/github/jordannegreiros/vendas/domain/entity/Cliente.java
@@ -2,12 +2,20 @@ package io.github.jordannegreiros.vendas.domain.entity;
 
 import lombok.*;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name = "CLIENTE")
 @NoArgsConstructor
 @AllArgsConstructor
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @Column(name = "nome", length = 100)
     private String name;
 
 }
