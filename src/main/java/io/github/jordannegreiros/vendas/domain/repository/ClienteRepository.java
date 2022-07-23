67bc812ce67be7780f94f53c7b858b55735cf5fa
jordan-negreiros <jordannegreirossantos@gmail.com>
2022-07-23 13:49:34 -0300

    jpql and native query

--- src/main/java/io/github/jordannegreiros/vendas/domain/repository/ClienteRepository.java
+++ src/main/java/io/github/jordannegreiros/vendas/domain/repository/ClienteRepository.java
@@ -2,6 +2,9 @@ package io.github.jordannegreiros.vendas.domain.repository;
 
 import io.github.jordannegreiros.vendas.domain.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,9 +12,17 @@ import java.util.List;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
-    List<Cliente> findByNomeLike(String name);
+    @Query(value = " select c from Cliente c where c.name like :name ")
+    List<Cliente> findByNameWithJpql(@Param("name") String name);
 
-    List<Cliente> findByNomeOrId(String name, Integer id);
+    @Query(value = " select * from CLIENTE where nome like '%:name%' ", nativeQuery = true)
+    List<Cliente> findByNameWithNativeSql(@Param("name") String name);
 
-    boolean existsByNome(String name);
+    @Modifying
+    @Query(value = " delete from Cliente c where c.name = :name")
+    void deleteByName(@Param("name") String name);
+
+    List<Cliente> findByNameOrId(String name, Integer id);
+
+    boolean existsByName(String name);
 }
