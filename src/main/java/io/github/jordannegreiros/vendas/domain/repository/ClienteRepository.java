9a0cec4ebd5d40830f41de492f2074d975369280
jordan-negreiros <jordannegreirossantos@gmail.com>
2022-07-23 13:38:26 -0300

    query method

--- src/main/java/io/github/jordannegreiros/vendas/domain/repository/ClienteRepository.java
+++ src/main/java/io/github/jordannegreiros/vendas/domain/repository/ClienteRepository.java
@@ -4,6 +4,14 @@ import io.github.jordannegreiros.vendas.domain.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+    List<Cliente> findByNomeLike(String name);
+
+    List<Cliente> findByNomeOrId(String name, Integer id);
+
+    boolean existsByNome(String name);
 }
