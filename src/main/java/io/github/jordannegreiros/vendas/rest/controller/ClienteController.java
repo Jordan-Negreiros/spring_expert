f364f1e0078abdf8b1256ae5e1b47d8baed630dc
jordan-negreiros <jordannegreirossantos@gmail.com>
2022-07-24 10:31:49 -0300

    test controller

---
+++ src/main/java/io/github/jordannegreiros/vendas/rest/controller/ClienteController.java
@@ -0,0 +1,17 @@
+package io.github.jordannegreiros.vendas.rest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/api/clientes")
+public class ClienteController {
+
+    @ResponseBody
+    @RequestMapping(value = "/hello/{nome}")
+    public String helloCliente(@PathVariable("nome") String nomeCliente) {
+        return String.format("Hello %s", nomeCliente);
+    }
+}
