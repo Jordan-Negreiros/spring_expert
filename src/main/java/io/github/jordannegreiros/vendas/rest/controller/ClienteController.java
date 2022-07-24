ec3718b6b47da6e03de720488cece4ae2c729dcb
jordan-negreiros <jordannegreirossantos@gmail.com>
2022-07-24 11:04:44 -0300

    post client

--- src/main/java/io/github/jordannegreiros/vendas/rest/controller/ClienteController.java
+++ src/main/java/io/github/jordannegreiros/vendas/rest/controller/ClienteController.java
@@ -4,12 +4,10 @@ import io.github.jordannegreiros.vendas.domain.ClienteNotFoundException;
 import io.github.jordannegreiros.vendas.domain.entity.Cliente;
 import io.github.jordannegreiros.vendas.domain.repository.ClienteRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/api/clientes")
@@ -18,11 +16,18 @@ public class ClienteController {
 
     private final ClienteRepository clienteRepository;
 
-    @ResponseBody
     @GetMapping("/{id}")
+    @ResponseBody
     public ResponseEntity<Cliente> getClienteById(@PathVariable Integer id) {
         var cliente = clienteRepository.findById(id)
             .orElseThrow(ClienteNotFoundException::new);
         return ResponseEntity.ok(cliente);
+    }
+
+    @PostMapping
+    @ResponseBody
+    public ResponseEntity<Cliente> save(@RequestBody Cliente cliente) {
+        var clienteSaved = clienteRepository.save(cliente);
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteSaved);
     }
 }
