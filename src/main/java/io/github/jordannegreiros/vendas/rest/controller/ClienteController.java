e7221a066bd80b75337a1d1c898e1e486a4cba6b
jordan-negreiros <jordannegreirossantos@gmail.com>
2022-07-24 10:39:20 -0300

    produce and consumes

--- src/main/java/io/github/jordannegreiros/vendas/rest/controller/ClienteController.java
+++ src/main/java/io/github/jordannegreiros/vendas/rest/controller/ClienteController.java
@@ -1,5 +1,6 @@
 package io.github.jordannegreiros.vendas.rest.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,7 +12,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class ClienteController {
 
     @ResponseBody
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+    @RequestMapping(
+        value = "/hello/{nome}",
+        method = RequestMethod.GET,
+        consumes = {MediaType.APPLICATION_JSON_VALUE},
+        produces = {MediaType.APPLICATION_JSON_VALUE}
+    )
     public String helloCliente(@PathVariable("nome") String nomeCliente) {
         return String.format("Hello %s", nomeCliente);
     }
