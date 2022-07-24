11335ba2e11a017ba741a14eb6b7f3df83059b03
jordan-negreiros <jordannegreirossantos@gmail.com>
2022-07-24 10:32:22 -0300

    request method

--- src/main/java/io/github/jordannegreiros/vendas/rest/controller/ClienteController.java
+++ src/main/java/io/github/jordannegreiros/vendas/rest/controller/ClienteController.java
@@ -3,6 +3,7 @@ package io.github.jordannegreiros.vendas.rest.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class ClienteController {
 
     @ResponseBody
-    @RequestMapping(value = "/hello/{nome}")
+    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
     public String helloCliente(@PathVariable("nome") String nomeCliente) {
         return String.format("Hello %s", nomeCliente);
     }
